59fd420ee776cb595cc0dbc513735054d143f3c1
Benjamin Zhao <Benjamin.zhao1995@hotmail.com>
2016-06-28 17:32:44 -0400

    Added templates, small fix to mobile site carousel changing height.

--- dotstar-website/public/js/index/ngRouter.js
+++ dotstar-website/public/js/index/ngRouter.js
@@ -22,3 +22,34 @@ myRoute.config(function ($routeProvider, $locationProvider){
 	});
 	$locationProvider.html5Mode(true);
 });
+
+//DEV template routing
+myRoute.config(function ($routeProvider, $locationProvider){
+	$routeProvider.
+	when('/dev/template1',{
+		templateUrl: '/views/dev/partials/template1.html',
+		controller: 'mainController',
+		controllerAs: 'main'
+	});
+	$locationProvider.html5Mode(true);
+});
+
+myRoute.config(function ($routeProvider, $locationProvider){
+	$routeProvider.
+	when('/dev/template2',{
+		templateUrl: '/views/dev/partials/template2.html',
+		controller: 'mainController',
+		controllerAs: 'main'
+	});
+	$locationProvider.html5Mode(true);
+});
+
+myRoute.config(function ($routeProvider, $locationProvider){
+	$routeProvider.
+	when('/dev/template3',{
+		templateUrl: '/views/dev/partials/template3.html',
+		controller: 'mainController',
+		controllerAs: 'main'
+	});
+	$locationProvider.html5Mode(true);
+});
